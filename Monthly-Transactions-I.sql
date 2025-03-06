45a7a2a41b953c75015d87d839b3226a80936596
Raunak Jaiswal <raunakjaiswal6102@gmail.com>
2025-03-06 23:14:17 +0530

    [1193] [Time Beats: 39.38%] - LeetPush

---
+++ Monthly-Transactions-I.sql
@@ -0,0 +1,8 @@
+select substring(trans_date,1,7) as month,
+        country,    
+        count(state) as trans_count,
+        sum(state = 'approved') as approved_count,
+        sum(amount) as trans_total_amount,
+        sum(if(state='approved',amount,0)) as approved_total_amount
+from Transactions
+group by month, country; 
